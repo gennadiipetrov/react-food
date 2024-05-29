9355e71a5719188027a7a5870f8081aee450d092
gennadiipetrov <77464892+gennadiipetrov@users.noreply.github.com>
2024-05-29 22:32:30 +0300

    fix remark

--- react-food/src/shared/use-count/UseCount.jsx
+++ react-food/src/shared/use-count/UseCount.jsx
@@ -4,11 +4,15 @@ import { useState } from "react";
 export const useCount = ({ initialValue = 0, min = 0, max = 5, step = 1 } = {}) => {
     const [count, setCount] = useState(initialValue);
 
+    const increment = useCallback(() => setCount(val => val === max ? val : val + step), [max, step]);
+    const decrement = useCallback(() => setCount(val => val === min ? val : val - step), [min, step]);
+    const reset = useCallback(() => setCount(initialValue), [initialValue]);
+
     return { 
       count,
-      reset: useCallback(() => setCount(initialValue), [initialValue]), 
-      increment: useCallback(() => setCount(val => val === max ? val : val + step), [max, step]), 
-      decrement: useCallback(() => setCount(val => val === min ? val : val - step), [min, step]),
+      reset, 
+      increment, 
+      decrement,
       MAX: max 
     };
 };
